c0cfcd2ba6f974c8e889114129f8c73bd2f59284
Runge, Jan <J.Runge@wertgarantie.com>
2020-01-06 17:17:26 +0100

    neue aufgaben mit onat

--- first.c
+++ first.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
+
 void printInt(int input)
 {
    char meinString[12] ;
@@ -8,6 +10,11 @@ void printInt(int input)
    sprintf(meinString, "%d", input);
    printf(meinString);
 }
+void printFloat(float f ){
+printf("%.6f", f);
+
+}
+
 void meineFunktion(int x, int f)
 {
    int ergebnis;
@@ -30,18 +37,17 @@ void meineFunktion(int x, int f)
 void meinsfunktion(int x)
 {
    printInt(x);
-   int meinsvariable = x;
-
-   if(x%5==0){
-      printf("bazz");
-   }
-
    if(x%3==0)
    {
       printf("fizz");
    }
+   if(x%5==0){
+      printf("bazz");
+   }
    printf("\n");
 }
+
+
 char CallClosesElevator(int pos_a, int pos_b, int pos_c, int caller)
 {
    int abstand_a= abs(pos_a - caller);
@@ -50,22 +56,94 @@ char CallClosesElevator(int pos_a, int pos_b, int pos_c, int caller)
 
    if(abstand_a<= abstand_b && abstand_a <=abstand_c){
       return 'a';
-
    }
    else if(abstand_b <= abstand_a && abstand_b <= abstand_c){
       return 'b';
-
    }
    else if(abstand_c <= abstand_a && abstand_c <= abstand_b){
       return 'c';
    }
 
 }  
+void zeigertest(){
+
+   int facharbeit_text = 7;
+   int *facharbeit_zeiger;
+   facharbeit_zeiger = &facharbeit_text;
+   facharbeit_text=12;
+   wertErhalten(facharbeit_zeiger);
+   printInt(*facharbeit_zeiger);
+}
+void wertErhalten(int *zeiger){
+   printInt(*zeiger);
+   *zeiger=16;
+}
+
+void zeigertest_klassich(){
+
+   int facharbeit_text = 12;
+   wertErhalten(facharbeit_text);
+   printInt(facharbeit_text);
+}
+void wertErhalten_klassisch(int wert){
+   printInt(wert);
+   wert=16;
+}
+void AddComplex(float reA, float imA,
+               float reB, float imB,
+               float *reR, float *imR)
+{
+   float realteil=reA + reB;
+   float imteil=imA + imB;
+   *reR=realteil;
+   *imR=imteil;
+}
+
+void MultiplyComplex(float reA, float imA,
+               float reB, float imB,
+               float *reR, float *imR)
+{
+//(a+b*i) * (c+d*i) 
+//(a+b*i) * (c+d*i) 
+//a=reA
+//b=imA
+//c=reB
+//d=imB
+   float rteil=reA*reB - imA*imB;
+   float iteil=reA*imB + reB*imA;
+   *reR=rteil;
+   *imR=iteil;
+}
+
+               
+
+
+
 
 int main()
 {
-   printf("%c",  CallClosesElevator(3,6,9,7));
+   /*
+   
+   */
+   // printf("%c",  CallClosesElevator(3,6,9,7));
+   zeigertest();
+   float ergRealteil;
+   float ergImteil;
+   float *zeigerReal =&ergRealteil;
+   float *zeigerImag =&ergImteil;
+
+
+   AddComplex(13,2.5,
+   3.3, 14, 
+   zeigerReal, zeigerImag
+   );
+   printFloat(ergRealteil);
+   printFloat(ergImteil);
+
+
 }
+
+
 
 
 
